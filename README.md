13d90b25030392f9a55d17e31bb5696cf3a9ef7a
xianhuawei <xianhuawei.zhcn@qq.com>
2017-12-17 20:45:05 +0800

    init

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ```
   wget https://raw.githubusercontent.com/xiaomatech/nginx/master/nginx-1.12.0-1.x86_64.rpm
   yum localinstall -y ./nginx-1.12.0-1.x86_64.rpm
+  yum install -y GeoIP GeoIP-data 
   #geoipupdate
   
   修改 nginx配置 比如:/opt/nginx/conf/common/ssl.conf 的ssl 路径等
--- lua/etl/location.lua
+++ lua/etl/location.lua
@@ -2,3 +2,7 @@ local ipdatx = require "resty.ipdatx"
 local ok, err = ipdatx.init(ipdatx_path)
 local ipip, err = ipdatx:query(ngx.var.remote_addr)
 local ip2location = ngx.shared.ip2location
+
+-- local geo = require 'resty.maxminddb'
+-- local maxm = geo.new("/path/to/GeoLite2-City.mmdb")
+-- local res,err = maxm:lookup(ngx.var.arg_ip or ngx.svar.remote_addr)
